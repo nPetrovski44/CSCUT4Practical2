fd40c37fa236da980da68071d29d9663458c0dd1
Nikolay Petrovski <nmp00008@students.sitr.ac.uk>
2021-02-17 16:17:33 +0200

    Working on the initial steps 1 and 2 to capitalise names and format dates.

--- FilesInOut.java
+++ FilesInOut.java
@@ -3,32 +3,92 @@ import java.awt.*;
 import java.awt.event.*;
 import java.util.*;
 import javax.swing.*;
-import java.lang.Number;
 
+
+import java.lang.Number;
+import java.text.SimpleDateFormat;
 /**
  * 
  * CSCU9T4 Java strings and files exercise.
  *
  */
 public class FilesInOut {
+	
+	protected static ArrayList<Table> table = new ArrayList<Table>();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException 
+    {
         // Replace this with statements to set the file name (input) and file name (output).
         // Initially it will be easier to hardcode suitable file names.
-
+    	File input = new File("C:\\Users\\Nikolay Petrovski\\git\\CSCUT4Practical2\\inputm.txt");
+    	File output = new File("C:\\Users\\Nikolay Petrovski\\git\\CSCUT4Practical2\\formatted.txt");
+    	SimpleDateFormat dateType = new SimpleDateFormat("ddmmyyyy");
+    	SimpleDateFormat newDate = new SimpleDateFormat("dd/mm/yyyy");
+    	Date date = null;
+    	
         // Set up a new Scanner to read the input file.
-        // Processing line by line would be sensible here.
-        // Initially, echo the text to System.out to check you are reading correctly.
+    	
+    	FileWriter writer = new FileWriter(output);
+    	Scanner scan = new Scanner(input);
+    	scan.useDelimiter("\n");
+    	
+// Initially, echo the text to System.out to check you are reading correctly.
+    	while(scan.hasNext())
+    	{
+            String line = scan.next();
+            String lineDate = "";
+            String lineName = "";
+            Scanner lineScan = new Scanner(line);
+            while(lineScan.hasNext())
+            {
+                String word = lineScan.next();
+                try
+                {
+                	date = dateType.parse(word);
+                	String formatedDate = newDate.format(date);
+                	lineDate = formatedDate;
+                	
+                }catch(Exception e)
+                {
+                	if(word.length() == 1)
+                	{
+                		word += ".";
+                	}
+                	lineName += Character.toUpperCase(word.charAt(0)) + word.substring(1) + " ";
+                }
+            }
+            try
+            {
+            	Table t = new Table(lineName.trim(), newDate.parse(lineDate));
+                table.add(t);
+            }
+            catch(Exception e) {}
+            writer.write(lineName.trim() + " " + lineDate + "\n");
+    		System.out.printf("%-25s %s\n", lineName.trim(), lineDate);
+    	}
+    	writer.close();
+    	
+    	//print();
         // Then add code to modify the text to the output format.
 
         // Set up a new PrintWriter to write the output file.
         // Add suitable code into the above processing (because you need to do this line by line also.
         // That is, read a line, write a line, loop.
 
+    	
         // Finally, add code to read the filenames as arguments from the command line.
 
-        System.out.println("You need to add your own code to do anything");
 
     } // main
+    
+    public static void print()
+    {
+    	ListIterator<Table> iter = table.listIterator();
+    	while(iter.hasNext())
+    	{
+    		Table current = iter.next();
+    		System.out.println(current.getName() + " " + current.getDate());
+    	}
+    }
 
 } // FilesInOut
---
+++ Table.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+import java.lang.Number;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+/**
+ * 
+ * CSCU9T4 Java strings and files exercise.
+ *
+ */
+public class Table {
+	private String name;
+	private Date date;
+	private SimpleDateFormat newDate = new SimpleDateFormat("dd/mm/yyyy");
+    public Table(String n, Date d)
+    {
+    	this.name = n;
+    	this.date = d;	
+    }
+
+    public String getName()
+    {
+    	return this.name;
+    }
+    
+    public Date getDate()
+    {
+    	return this.date;
+    }
+} // FilesInOut
